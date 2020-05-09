23132a086e61a0384251def4f134a62d6c946cc6
Matheus Frauches <frauches.27@gmail.com>
2020-05-09 17:29:45 -0300

    Finish Challenge

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from 'react';
+import income from '../../assets/income.svg';
+import outcome from '../../assets/outcome.svg';
+import total from '../../assets/total.svg';
+import Header from '../../components/Header';
+import api from '../../services/api';
+import formatValue from '../../utils/formatValue';
+import { Card, CardContainer, Container, TableContainer } from './styles';
+
+interface Transaction {
+  id: string;
+  title: string;
+  value: number;
+  formattedValue: string;
+  formattedDate: string;
+  type: 'income' | 'outcome';
+  category: { title: string };
+  created_at: Date;
+}
+
+interface Balance {
+  income: string;
+  outcome: string;
+  total: string;
+}
+
+interface TransactionsBalance {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+const Dashboard: React.FC = () => {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({
+    income: '0',
+    outcome: '0',
+    total: '0',
+  } as Balance);
+
+  useEffect(() => {
+    async function loadTransactions(): Promise<void> {
+      const { data } = await api.get<TransactionsBalance>('/transactions');
+
+      setTransactions(data.transactions);
+      setBalance(data.balance);
+    }
+
+    loadTransactions();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <CardContainer>
+          <Card>
+            <header>
+              <p>Entradas</p>
+              <img src={income} alt="Income" />
+            </header>
+            <h1 data-testid="balance-income">
+              {formatValue(Number(balance.income))}
+            </h1>
+          </Card>
+          <Card>
+            <header>
+              <p>Saídas</p>
+              <img src={outcome} alt="Outcome" />
+            </header>
+            <h1 data-testid="balance-outcome">
+              {formatValue(Number(balance.outcome))}
+            </h1>
+          </Card>
+          <Card total>
+            <header>
+              <p>Total</p>
+              <img src={total} alt="Total" />
+            </header>
+            <h1 data-testid="balance-total">
+              {formatValue(Number(balance.total))}
+            </h1>
+          </Card>
+        </CardContainer>
+
+        <TableContainer>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'outcome' ? '- ' : ''}
+                    {formatValue(Number(transaction.value))}
+                  </td>
+                  <td>{transaction.category.title}</td>
+                  <td>{transaction.formattedDate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </TableContainer>
+      </Container>
+    </>
+  );
+};
+
+export default Dashboard;
